a51d711c63b53b167e47237763f905d9cc2613bd
Shelby Polasek <shelbypolasek@gmail.com>
2020-04-27 20:40:19 -0500

    loops practice

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,29 +1,64 @@
 'use strict';
 
+//
+// function userPrompt(){
+//
+// Number(prompt('Enter odd # betwee 1-50'));
+// }
+//
+//
+// var i;
+// while ( (i < 50) && (i % 2 !== 0) ){
+//     if(i !== userPrompt()){
+//         userPrompt();
+//         break;
+//     } i += 2;
+// }
 
-function rand(num) {
-    return 0 < num > 50;
-}
 
-function isOdd(num) {
-    return num % 2 !== 0;
-}
 
-function userPrompt(){
- return Number(prompt('Please enter an odd number between 1 and 50'));
-}
 
-function isOddAndBetween50() {
-    userPrompt();
-  if(rand(userPrompt() === false ) || isOdd(userPrompt()) === false ){
-          userPrompt();
-      }
-}
 
-function outputExecptUserInput() {
-    for (var i = 1; i < 50; i += 2) {
-        if (i % 2 !== 0) {
-            console.log('Here is an odd number : ' + i);
-        }
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function rand(num) {
+//     return 0 < num > 50;
+// }
+//
+// function isOdd(num) {
+//     return num % 2 !== 0;
+// }
+
+// function userPrompt(){
+//  return Number(prompt('Please enter an odd number between 1 and 50'));
+// }
+//
+//
+//
+// function isOddAndBetween50() {
+//     userPrompt();
+//   if(rand(userPrompt() === false ) || isOdd(userPrompt()) === false ){
+//           userPrompt();
+//       }
+// }
+//
+// isOddAndBetween50();
+//
+// function outputExecptUserInput() {
+//     for (var i = 1; i < 50; i += 2) {
+//         if (i % 2 !== 0) {
+//             console.log('Here is an odd number : ' + i);
+//         }
+//     }
+// }
---
+++ js/while.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//============== While ==============
+var i = 2;
+var powerOfTwo = Math.pow(i, 17);
+while(i < powerOfTwo ){
+    console.log(i);
+    i *= 2;
+}
+
+//=========== Do While ==============
+
+var randFifty = Math.floor(Math.random()*50)+ 50;
+
+fo
