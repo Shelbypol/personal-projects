4b4059dd6262e1e16ef4850114830453f19fcf33
Shelby Polasek <shelbypolasek@gmail.com>
2020-04-29 20:59:47 -0500

    bonus loops exercises

---
+++ js/loops_bonus.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// For each of the following outputs, create a while loop, do-while loop, and for loop to duplicate the output...
+
+
+// ------- 1 --------
+// 10
+// 8
+// 6
+// 4
+// 2
+// 0
+// -2
+//==================== while
+// var i = 10;
+// while(i !== -4){
+//     console.log(i);
+//     i -= 2;
+// }
+
+//==================== do-while
+
+// var i = 10;
+// do{
+//     console.log(i);
+//     i -= 2;
+// }while( i !== -4);
+
+//==================== for loop
+
+for(var i = 10; i > -4; i -=2){
+    console.log(i);
+}
+
+
+
+// ------- 2 --------
+// 2
+// 4
+// 16
+// 256
+// 65536
+//
+// ---------------
+
+
+
+
+//==================== while
+
+
+
+
+//==================== do-while
+
+
+
+
+//==================== for loop
