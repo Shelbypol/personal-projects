2a71f9b45936aadbbd0703c5a18c9a0da421e4dd
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-16 08:59:48 -0500

    animation

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,6 +1,30 @@
 'use strict';
 
 // Wrap every letter in a span
+let textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+    .add({
+        targets: '.ml12 .letter',
+        translateX: [0,40],
+        translateZ: 0,
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 1000,
+        delay: (el, i) => 50 * i
+    }).add({
+    targets: '.ml12 .letter',
+    translateX: [-30,0],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1000,
+    delay: (el, i) => 50  * i
+});
+
+// $(".typingAnimation").css("transform", "rotate(90deg");
+
+// Wrap every letter in a span
 // var textWrapper = document.querySelector('.ml3');
 // textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 //
@@ -18,25 +42,3 @@
 //     easing: "easeOutExpo",
 //     delay: 1000
 // });
-
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml12');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-    .add({
-        targets: '.ml12 .letter',
-        translateX: [40,0],
-        translateZ: 0,
-        opacity: [0,1],
-        easing: "easeOutExpo",
-        duration: 1200,
-        delay: (el, i) => 500 + 30 * i
-    }).add({
-    targets: '.ml12 .letter',
-    translateX: [0,-30],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 1100,
-    delay: (el, i) => 100 + 30 * i
-});
