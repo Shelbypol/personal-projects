0aa52f0f79885d93ad914bc319d9cb332f5190b5
Shelby Polasek <shelbypolasek@gmail.com>
2020-07-06 11:27:14 -0500

    working hilltop

---
+++ hilltop-liquors-store/HilltopCheckout.java
@@ -0,0 +1,12 @@
+public class HilltopCheckout {
+
+    //Menu OPTION
+//    1) browse everything
+//    2) browse beer
+//           - SUB-MENU
+//
+//    3) browse liquor
+//    4) browse wine
+
+
+}
